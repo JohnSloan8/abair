437b79ddb523ad59679394c0da22f3fdbb8bcd0a
john <johnsloan1984@gmail.com>
2022-09-22 18:29:16 +0100

    removed unnecessary outer flexbox

--- src/routes/Pages/Pages.tsx
+++ src/routes/Pages/Pages.tsx
@@ -2,23 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
 
-import { CenteredFlexBox } from '@/components/styled';
-
 import routes from '..';
 
 // import { getPageHeight } from './utils';
 
 function Pages() {
   return (
-    <CenteredFlexBox>
-      <Box sx={{ width: '100%' }} justifyContent="center">
-        <Routes>
-          {Object.values(routes).map(({ path, component: Component }) => {
-            return <Route key={path} path={path} element={<Component />} />;
-          })}
-        </Routes>
-      </Box>
-    </CenteredFlexBox>
+    <Box sx={{ width: '100%' }} justifyContent="center">
+      <Routes>
+        {Object.values(routes).map(({ path, component: Component }) => {
+          return <Route key={path} path={path} element={<Component />} />;
+        })}
+      </Routes>
+    </Box>
   );
 }
 
