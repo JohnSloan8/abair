228707287dce70cc548d2ff972be9abbca0bc689
john <johnsloan1984@gmail.com>
2023-02-14 13:56:12 +0000

    levelled out textboxes on front page

--- src/display/controllers/Recognition/RecognitionImage/RecognitionImage.tsx
+++ src/display/controllers/Recognition/RecognitionImage/RecognitionImage.tsx
@@ -1,24 +1,39 @@
-// import { useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
+
 import Box from '@mui/material/Box';
 
 import Image from 'mui-image';
 
 import { CenteredFlexBox } from '@/display/utils/flex';
+import { frontPageSelectionBoxSize } from '@/store/viewDimensions';
 
-// import { frontPageSelectionBoxSize } from '@/store/viewDimensions';
 import womanSpeakingLogo from '/assets/images/misc/woman-speaking-into-phone.png';
 
 const RecognitionImage = () => {
-  // const frontPageSelectionBoxSizeValue = useRecoilValue(frontPageSelectionBoxSize);
+  const frontPageSelectionBoxSizeValue = useRecoilValue(frontPageSelectionBoxSize);
 
   return (
-    <Box sx={{ width: '100%', position: 'relative' }} height={455}>
+    <Box
+      sx={{
+        width: '100%',
+        position: 'relative',
+      }}
+      minHeight={280}
+    >
       <CenteredFlexBox>
-        <Box sx={{ position: 'absolute', bottom: 0, height: '100%' }}>
+        <Box
+          sx={{
+            // position: 'absolute',
+            bottom: 0,
+            height: {
+              sm: frontPageSelectionBoxSizeValue - 75,
+              xs: frontPageSelectionBoxSizeValue - 65,
+            },
+          }}
+          minHeight={200}
+        >
           <Image
             duration={0}
-            // height={'100%'}
-            // width={'100%'}
             easing="ease-out"
             alt={`woman speaking into phone`}
             src={womanSpeakingLogo}
