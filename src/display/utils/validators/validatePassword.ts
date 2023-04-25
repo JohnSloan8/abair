d0a9d673d3d634b39d577bdcfdd879278fc70137
john <johnsloan1984@gmail.com>
2023-04-25 10:28:28 +0100

    changed passwor dregex

--- src/display/utils/validators/validatePassword.ts
+++ src/display/utils/validators/validatePassword.ts
@@ -1,9 +1,10 @@
-const passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+// const passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[()"^@$!%*?&])[A-Za-z\d()"^@$!%*?&]{8,}$/;
+const passwordRe = /^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$/;
 const validatePassword = (password: string) => {
   if (String(password).match(passwordRe) !== null) {
-    return true;
+    return false;
   }
-  return false;
+  return true;
 };
 
 export default validatePassword;
